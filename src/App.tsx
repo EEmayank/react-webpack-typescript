dc4404036e7dfe89adecd5657675208d3a0dccda
EEmayank <34167705+EEmayank@users.noreply.github.com>
2022-02-26 11:04:39 +0530

    counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Counter } from './Counter'
 import './style.css'
 import IMAGE from './download.jpg'
 
 export const App = () => {
+    
     return (
         <>
         <h1>nice website bro! [{process.env.NODE_ENV}]</h1>
         <img src={IMAGE} alt='doge slav'/>
+        <Counter />
         </>
     )
 }
