d2ec70df64c273514b88ace28be67e2d213de1c0
EEmayank <34167705+EEmayank@users.noreply.github.com>
2022-02-27 18:02:45 +0530

    first format run

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Counter } from './Counter'
-import './style.css'
-import IMAGE from './download.jpg'
+import { Counter } from './Counter';
+import './style.css';
+import IMAGE from './download.jpg';
 
 export const App = () => {
-    
-    return (
-        <>
-        <h1>nice website bro! [{process.env.NODE_ENV}]</h1>
-        <img src={IMAGE} alt='doge slav'/>
-        <Counter />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>nice website bro! [{process.env.NODE_ENV}]</h1>
+      <img src={IMAGE} alt="doge slav" />
+      <Counter />
+    </>
+  );
+};
